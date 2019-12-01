ea3630bad7743c30e154d002ce0dd783163f8beb
Rainangramacho <rai_bomfim@hotmail.com>
2019-12-01 18:57:51 -0300

    fixing time troubles

--- rna.js
+++ rna.js
@@ -16,9 +16,10 @@ output: outputLayer
 var learningRate = .15;
 
 for (var i = 0; i < 2000000; i++) {  
+     
     myNetwork.activate([15,20]);  
     myNetwork.propagate(learningRate, [0]);
-    
+     
     myNetwork.activate([25,25]);  
     myNetwork.propagate(learningRate, [0]);
   
@@ -30,10 +31,10 @@ for (var i = 0; i < 2000000; i++) {
   
     myNetwork.activate([20,20]);  
     myNetwork.propagate(learningRate, [0]);
-  
+   
     myNetwork.activate([30,28]);  
     myNetwork.propagate(learningRate, [0]);
-  
+   
     myNetwork.activate([31,19]);  
     myNetwork.propagate(learningRate, [0]);
   
@@ -42,7 +43,7 @@ for (var i = 0; i < 2000000; i++) {
   
     myNetwork.activate([40,35]);  
     myNetwork.propagate(learningRate, [1]);
-  
+   
     myNetwork.activate([70,60]);  
     myNetwork.propagate(learningRate, [1]);
   
@@ -71,14 +72,14 @@ for (var i = 0; i < 2000000; i++) {
         output: process.stdout
     });
 
-    rl.question('Digite sua idade : ', (idade) => {
-        var start = new Date().getTime();
-
+    rl.question('Digite sua idade : ', (answer1) => {
+        var start = new Date().getTime()/1000;
+        
         rl.question('Digite a frase solicitada : ', (answer2) => {
-            var end = new Date().getTime();
-            var tempo_gasto = (1000 /(end - start));
-            console.log(Math.round(myNetwork.activate([idade,tempo_gasto])));
-            console.log(tempo_gasto.toFixed(2) + "s");
+            var end = new Date().getTime()/1000;
+            var tempo_gasto = (end - start);
+            console.log(Math.round(myNetwork.activate([answer1,tempo_gasto])));
+            console.log(tempo_gasto.toFixed(0) + "s");
             rl.close();
         });
     });
