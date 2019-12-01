dcc1d071f979cf1519b456efd7c44ebdf2cff8bb
Rainangramacho <rai_bomfim@hotmail.com>
2019-12-01 16:26:22 -0300

    Initial commit

---
+++ rna.js
@@ -0,0 +1,95 @@
+const {Layer, Network} = require('synaptic');
+const readline = require('readline');
+var inputLayer = new Layer(2);
+var hiddenLayer = new Layer(3);
+var outputLayer = new Layer(1);
+
+inputLayer.project(hiddenLayer);
+hiddenLayer.project(outputLayer);
+
+var myNetwork = new Network({
+input: inputLayer,
+hidden: [hiddenLayer],
+output: outputLayer
+});
+
+var learningRate = .15;
+
+for (var i = 0; i < 2000000; i++) {  
+    // 0,0 => 0  
+    myNetwork.activate([15,20]);  
+    myNetwork.propagate(learningRate, [0]);
+    // 0,1 => 1  
+    myNetwork.activate([25,25]);  
+    myNetwork.propagate(learningRate, [0]);
+  
+    myNetwork.activate([90,10]);  
+    myNetwork.propagate(learningRate, [0]);
+  
+    myNetwork.activate([80,15]);  
+    myNetwork.propagate(learningRate, [0]);
+  
+    myNetwork.activate([20,20]);  
+    myNetwork.propagate(learningRate, [0]);
+  
+    // 0,1 => 1  
+    myNetwork.activate([30,28]);  
+    myNetwork.propagate(learningRate, [0]);
+  
+    // 1,1 => 0  
+    myNetwork.activate([31,19]);  
+    myNetwork.propagate(learningRate, [0]);
+  
+    myNetwork.activate([30,18]);  
+    myNetwork.propagate(learningRate, [0]);
+  
+    // 1,0 => 1  
+    myNetwork.activate([40,35]);  
+    myNetwork.propagate(learningRate, [1]);
+  
+    // 1,1 => 0  
+    myNetwork.activate([70,60]);  
+    myNetwork.propagate(learningRate, [1]);
+  
+    myNetwork.activate([60,58]);  
+    myNetwork.propagate(learningRate, [1]);
+  
+    // 1,1 => 0  
+    myNetwork.activate([65,59]);  
+    myNetwork.propagate(learningRate, [1]);
+    
+    myNetwork.activate([50,51]);  
+    myNetwork.propagate(learningRate, [1]);
+  
+    // 1,1 => 0  
+    myNetwork.activate([52,53]);  
+    myNetwork.propagate(learningRate, [1]);
+  
+    myNetwork.activate([15,90]);  
+    myNetwork.propagate(learningRate, [1]);
+  
+    myNetwork.activate([80,60]);  
+    myNetwork.propagate(learningRate, [1]);
+  
+  }
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.question('Digite sua idade : ', (answer1) => {
+        //var tempo_gasto = console.time("queryTime");
+
+        var start = new Date().getTime();
+        // var end = new Date().getTime();
+
+        rl.question('Digite a frase solicitada : ', (answer2) => {
+            // console.timeEnd("queryTime");
+            var end = new Date().getTime();
+            var tempo_gasto = (1000 /(end - start));
+            console.log(Math.round(myNetwork.activate([answer1,tempo_gasto])));
+            console.log(tempo_gasto.toFixed(2) + "s");
+            rl.close();
+        });
+    });
